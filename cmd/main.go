f5edf96873fb1f71c95e3542978a9531c48212a7
Alexei Lugovoi <alugovoi@mirantis.com>
2024-07-12 17:06:55 +0200

    Add clusterv1 into the scheme

--- cmd/main.go
+++ cmd/main.go
@@ -24,6 +24,8 @@ import (
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
@@ -45,6 +47,7 @@ func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
 	utilruntime.Must(infrastructurev1alpha1.AddToScheme(scheme))
+	utilruntime.Must(clusterv1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
